f650de331c9ad5e6141b0ffa01fbd6c8fc8de3e7
mdmjsh <joshharrison13@gmail.com>
2019-01-06 14:32:57 +0000

    q2 in writeup

--- select.sql
+++ select.sql
@@ -62,7 +62,7 @@ JOIN
    FROM acquistion
    WHERE status = 'completed')c
    ON c.child_company_id = f.child_company_id
-WHERE c.announced_date > f.announced_date;
+WHERE c.completion_date > f.announced_date;
 
  f)
 SELECT founder_id,
